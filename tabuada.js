0a255316f2110d9c4d61b2de0de76589ea64ccf5
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-07 16:15:00 -0300

    add tabuada changes

--- tabuada.js
+++ tabuada.js
@@ -6,16 +6,21 @@ function tabuada() {
     if (n == `` ||e == ``){
          window.alert(`Por favor preencha os campos solicitados`)
     }else {
-        var r = '' //variavel r esta vazia
-        var res = window.document.getElementById('resposta') //ela tem o valor do texto
-        //"Preencha os valores acima"
+        let r = 0; 
+        let res = window.document.getElementById('resposta') 
+        
         res.innerHTML = `a tabuada do ${n} é: <br>`
-        var x = 0 
-        do {
-            r= n*x
-            res.innerHTML += `${n}x${x} =${r} <br>`/* Esse mais com igual significa que ele mantém 
-            a resposta salva enquanto imprime outras */
-            x++
-        } while (x <= e)
+        for(i=0; i<=e; i++){
+            r = n*i
+            res.innerHTML += `${n}x${i}= ${r} <br>`
+        }
+
+        //outra forma de fazer
+        // var x = 0 
+        // do {
+        //     r= n*x
+        //     res.innerHTML += `${n}x${x} =${r} <br>`;
+        //     x++
+        // } while (x <= e)
     }
 }
