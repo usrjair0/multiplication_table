67a60abde8436d0adb2027806104c302a39898f0
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-07 16:17:21 -0300

    some changes some coments

--- tabuada.js
+++ tabuada.js
@@ -1,18 +1,19 @@
 function tabuada() {
-    var num = window.document.getElementById('num').value 
-    var end = document.getElementById('end').value
-    var n = Number(num) //pegando o valor numerico de "num"
-    var e = Number(end) //pegando o valor numerico de "end"
-    if (n == `` ||e == ``){
-         window.alert(`Por favor preencha os campos solicitados`)
-    }else {
+
+    var num = window.document.getElementById('num').value; 
+    var end = document.getElementById('end').value;
+    var n = Number(num);
+    var e = Number(end);
+
+    if (n == `` ||e == ``)
+        window.alert(`Por favor preencha os campos solicitados`)
+    else {
         let r = 0; 
-        let res = window.document.getElementById('resposta') 
-        
-        res.innerHTML = `a tabuada do ${n} é: <br>`
+        let res = window.document.getElementById('resposta'); 
+        res.innerHTML = `a tabuada do ${n} é: <br>`;
         for(i=0; i<=e; i++){
-            r = n*i
-            res.innerHTML += `${n}x${i}= ${r} <br>`
+            r = n*i;
+            res.innerHTML += `${n}x${i}= ${r} <br>`;
         }
 
         //outra forma de fazer
